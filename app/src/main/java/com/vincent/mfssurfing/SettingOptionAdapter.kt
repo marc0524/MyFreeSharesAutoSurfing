3bbde9a44ce9353ecadc4c6b3a1ee3707eaca9b7
Vincent <eric61011@gmail.com>
2019-02-08 21:59:03 +0800

    Use ExpandableListView in setting page and display default ignored ads.

--- app/src/main/java/com/vincent/mfssurfing/SettingOptionAdapter.kt
+++ app/src/main/java/com/vincent/mfssurfing/SettingOptionAdapter.kt
@@ -4,16 +4,26 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 
-class SettingOptionAdapter(
-    val context: Context,
-    val optionViews: List<SettingOptionView>) : BaseAdapter() {
+class SettingOptionAdapter (
+    private val context: Context,
+    private val groupViews: List<SettingOptionView>,
+    private val childTexts: List<List<String>>) : BaseExpandableListAdapter() {
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+    override fun getGroup(groupPosition: Int): Any {
+        return groupViews.get(groupPosition)
+    }
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
+        return true
+    }
+
+    override fun hasStableIds(): Boolean {
+        return true
+    }
+
+    override fun getGroupView(groupPosition: Int, isExpanded: Boolean, convertView: View?, parent: ViewGroup?): View {
         val view: View
 
         if (convertView == null) {
@@ -27,24 +37,70 @@ class SettingOptionAdapter(
         val txtDescription = view.findViewById<TextView>(R.id.txtDescription)
         val widgetView = view.findViewById<FrameLayout>(R.id.frameLayout)
 
-        val optionView = optionViews.get(position)
+        val optionView = groupViews.get(groupPosition)
         txtTitle.text = optionView.title
         txtDescription.text = optionView.description
+
+        widgetView.removeAllViews()
         widgetView.addView(optionView.view)
 
         return view
     }
 
-    override fun getItem(position: Int): Any {
-        return optionViews.get(position)
+    override fun getChildrenCount(groupPosition: Int): Int {
+        return childTexts.get(groupPosition).size
     }
 
-    override fun getItemId(position: Int): Long {
-        return 0
+    override fun getChild(groupPosition: Int, childPosition: Int): Any {
+        return childTexts.get(groupPosition).get(childPosition)
     }
 
-    override fun getCount(): Int {
-        return optionViews.size
+    override fun getGroupId(groupPosition: Int): Long {
+        return groupPosition.toLong()
     }
+
+    override fun getChildView(
+        groupPosition: Int,
+        childPosition: Int,
+        isLastChild: Boolean,
+        convertView: View?,
+        parent: ViewGroup?
+    ): View {
+        val view: View
+
+        if (convertView == null) {
+            val inflater = LayoutInflater.from(context)
+            view = inflater.inflate(R.layout.list_child_setting, null)
+        } else {
+            view = convertView
+        }
+
+        val txtAdId = view.findViewById<TextView>(R.id.txtAdId)
+        txtAdId.text = childTexts.get(groupPosition).get(childPosition)
+
+        if (childPosition == 0) {
+            val layout = view.findViewById<RelativeLayout>(R.id.relativeLayout)
+            layout.setOnClickListener {
+                Toast.makeText(context, "Add", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            val imgDelete = view.findViewById<ImageView>(R.id.imgDelete)
+            imgDelete.setImageResource(android.R.drawable.ic_menu_close_clear_cancel)
+            imgDelete.setOnClickListener {
+                Toast.makeText(context, "Delete", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return view
+    }
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
+    }
+
+    override fun getGroupCount(): Int {
+        return groupViews.size
+    }
+
 
 }
